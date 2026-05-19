ebf69309be22fcaf2febd98fb741a8a97e6ba752
Skip <skip@usecurling.com>
2026-05-19 14:28:19 +0000

    Auto-commit after sync

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -101,4 +101,6 @@ export default function ForgotPassword() {
           )}
         </div>
       </div>
-    </div
+    </div>
+  )
+}
--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -104,4 +104,6 @@ export default function ResetPassword() {
           )}
         </div>
       </div>
-    <
+    </div>
+  )
+}
